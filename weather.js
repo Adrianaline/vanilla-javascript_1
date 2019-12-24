1a57387ec0067f51d8f6933527f3473b586ebef8
Adrianaline <47289975+Adrianaline@users.noreply.github.com>
2019-12-24 22:06:11 +0900

    3.10

--- weather.js
+++ weather.js
@@ -1,5 +1,17 @@
+const weather = document.querySelector('.js-weather');
+
 const API_KEY = '260fee4642516e862d01d3c5c6010ec9';
 const COORDS = 'coords';
+
+function getWeather(lat, lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`).then(function (response) {
+        return response.json();
+    }).then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `temperature ${temperature} @ ${place}`
+    });
+}
 
 function saveCoords(coordsOb) {
     localStorage.setItem(COORDS, JSON.stringify(coordsOb));
@@ -13,10 +25,11 @@ function handleGeoSuccess(position) {
         longitude
     };
     saveCoords(coordsOb);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
-    console.log("Can't access geo location")
+    console.log("Can't access geo location");
 }
 
 function askForCoords() {
@@ -24,11 +37,12 @@ function askForCoords() {
 }
 
 function loadCoords() {
-    const loadCoords = localStorage.getItem(COORDS);
-    if (loadCoords === null) {
+    const loadedCoords = localStorage.getItem(COORDS);
+    if (loadedCoords === null) {
         askForCoords();
     } else {
-        // getWeather();
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 
